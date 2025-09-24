6a8b5e4238a6f70b4db8d8024fa21df7aaeb0ec2
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-23 20:07:26 -0400

    move from callback functions to chained

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'pug');
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static('public'));
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static('public'))
 
-//CALL BACKS
-function getUsers(cb){
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) return cb(err);
-    const users = JSON.parse(data);
-    return cb(null, users);
-  });
+// PROMISES
+const getUsers = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile('data.json', 'utf-8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        const users = JSON.parse(data)
+        resolve(users)
+      }
+    })
+  })
 }
 
-app.get('/', (req,res) => {
-  
-}); 
+app.get('/', (req, res) => {
+  // Chained instead of nested functions
+  getUsers()
+    .then((users) => {
+      res.render('index', { title: 'Users', users: users.users })
+    })
+    .catch((err) => {
+      res.render('error', { error: err })
+    })
+})
 
-
-app.listen(3000, () => console.log('App listening on port 3000!'));
+app.listen(3000, () => console.log('App listening on port 3000!'))
